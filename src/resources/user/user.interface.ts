dd81ef9b2dfaaf0a8f3cd57943c3226a64e5b16c
Umoh Samuel <samuelwisdom360@gmail.com>
2024-01-23 16:17:03 +0100

    add base userService methods and user dao functions

--- src/resources/user/user.interface.ts
+++ src/resources/user/user.interface.ts
@@ -2,3 +2,22 @@ export interface IUser {
   username: string;
   password: string;
 }
+
+// *must match table columns name, snake_case!
+export type TUserFilter = {
+  id?: number;
+  email?: string;
+  username?: string;
+  nanoid?: string;
+}
+
+// *email, password column types used only in auth service and a single user.dao fn
+export type TUpdateUser = {
+  username?: string;
+  first_name?: string;
+  last_name?: string;
+}
+
+export type TFilterOptions = {
+  sensitiveFields?: boolean;
+}
--- src/resources/user/user.service.ts
+++ src/resources/user/user.service.ts
@@ -1,10 +1,34 @@
 import { HttpStatus, HttpException } from '@/utils/exceptions';
-import { create } from './user.dao';
+import { create, findOne, removeOne, updateOne } from './user.dao';
+import { IUser, TUserFilter, TFilterOptions, TUpdateUser } from './user.interface';
 
 class UserService {
   public async create(first_name: string, last_name: string, email: string, username: string) {
     return await create(first_name, last_name, email, username);
-  } 
+  }
+
+  public async isUser(filter: TUserFilter) {
+    const user = await findOne(filter);
+    if (!user)
+      return false;
+    return true
+  }
+
+  // auth flag to return sensitive fields
+  public async findUser(filter: TUserFilter) {
+    const user = await findOne(filter);
+    if (!user)
+      throw new HttpException(HttpStatus.NOT_FOUND, 'user not found');
+    return user;
+  }
+
+  public async updateUser(filter: TUserFilter, newData: TUpdateUser) {
+    return await updateOne(filter, newData);
+  }
+
+  public async deleteUser(filter: TUserFilter) {
+    return await removeOne(filter);
+  }
 }
 
 export { UserService };
