d30174881889dda1f3b986529b90d6f199a40eb2
Umoh Samuel <samuelwisdom360@gmail.com>
2024-01-24 19:36:17 +0100

    coerce id strings to numbers; use snake_case for object keys, to match database column names

--- src/resources/user/user.validators.ts
+++ src/resources/user/user.validators.ts
@@ -1,30 +1,31 @@
 import { z } from 'zod';
 
-const id = z.number();
+const id = z.coerce.number();
 const nanoid = z.string();
-const firstName = z.string().min(1);
-const lastName = z.string();
+const first_name = z.string().min(1);
+const last_name = z.string();
 const email = z.string().email();
 const username = z.string();
 
+// object key's case should match keys defined in interface (snake_case) which represent a table's column
 export const create = z.object({ 
-  firstName,
-  lastName,
+  first_name,
+  last_name,
   email,
   username
 });
 
 export const findQuery = z.object({ 
   id: id.optional(),
-  firstName: firstName.optional(),
-  lastName: lastName.optional(),
+  first_name: first_name.optional(),
+  last_name: last_name.optional(),
   email: email.optional(),
   username: username.optional()
  });
 
  export const updateFields = z.object({ 
-  firstName: firstName.optional(),
-  lastName: lastName.optional(),
+  first_name: first_name.optional(),
+  last_name: last_name.optional(),
   email: email.optional(),
   username: username.optional()
  });
